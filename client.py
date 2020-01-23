7a92fc7625912c6426d84870c14da9315d06e6da
David Pich <davidpich.kh@gmail.com>
2020-01-23 19:50:47 +0900

    Modify client.py

--- client.py
+++ client.py
@@ -6,6 +6,31 @@ import pickle
 import struct
 import time
 
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def cvDrawBoxes(detections, img):
+    for detection in detections:
+        x, y, w, h = detection[2][0],\
+            detection[2][1],\
+            detection[2][2],\
+            detection[2][3]
+        xmin, ymin, xmax, ymax = convertBack(
+            float(x), float(y), float(w), float(h))
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        cv2.putText(img,
+                    detection[0].decode() +
+                    " [" + str(round(detection[1] * 100, 2)) + "]",
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [0, 255, 0], 2)
+    return img
+
 def main():
     cap=cv2.VideoCapture(0)
     clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -15,14 +40,16 @@ def main():
     while True:
         ret,frame = cap.read()
         frame = cv2.resize(frame, (480, 320))
-        display(frame)
         #sent_time = time.time()
         #print(f'Frame #{count}: Send:{sent_time}')
         count += 1
         data = pickle.dumps(frame) 
         clientsocket.sendall(struct.pack("L", len(data))+data) 
         data = clientsocket.recv(1024)
-        print('Received', repr(data))
+        detections = pickle.loads(data)
+        image = cvDrawBoxes(detections, frame)
+        display(image)
+        print('Received', repr(detections))
         
 def display(frame):
     cv2.imshow("Client", frame)

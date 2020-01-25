aa7ec06bac827be93bd23972fee723c26730a3f2
David Pich <davidpich.kh@gmail.com>
2020-01-25 14:16:28 +0900

    Add server_fusion.py

---
+++ server_fusion.py
@@ -0,0 +1,160 @@
+from ctypes import *
+import math
+import random
+import os
+import cv2
+import numpy as np
+import time
+import darknet
+import socket
+import sys
+import pickle
+import struct 
+
+netMain = None
+metaMain = None
+altNames = None
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def cvDrawBoxes(detections, img):
+    for detection in detections:
+        x, y, w, h = detection[2][0],\
+            detection[2][1],\
+            detection[2][2],\
+            detection[2][3]
+        xmin, ymin, xmax, ymax = convertBack(
+            float(x), float(y), float(w), float(h))
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        cv2.putText(img,
+                    detection[0].decode() +
+                    " [" + str(round(detection[1] * 100, 2)) + "]",
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [0, 255, 0], 2)
+    return img
+
+def YOLO():
+    
+    global metaMain, netMain, altNames
+    configPath = "./cfg/yolov3-tiny-prn.cfg"
+    weightPath = "./yolov3-tiny-prn.weights"
+    metaPath = "./cfg/coco.data"
+    
+    HOST='192.168.100.126'
+    PORT= 5000
+    WIN_NAME = 'Server Display'
+    
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+         
+    print("Starting the YOLO loop...")
+
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+
+    serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('Socket created')
+
+    serversocket.bind((HOST,PORT))
+    print('Socket bind complete')
+    serversocket.listen(10)
+    print('Socket now listening')
+
+    conn,addr=serversocket.accept()
+
+    data = b''
+    payload_size = struct.calcsize("L") 
+    count = 0
+    while True:
+        
+        #------------------
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame_read = pickle.loads(frame_data)
+        #-----------------------
+        
+        # Received timestamp------
+        received_time = time.time()
+        print(f'Frame#{count}:Received@{received_time}')
+        #---------------------------------
+        
+        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb,
+                                   (darknet.network_width(netMain),
+                                    darknet.network_height(netMain)),
+                                   interpolation=cv2.INTER_LINEAR)
+
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        meta = pickle.dumps(detections)
+        
+        # Sent back timestamp------
+        sent_time = time.time()
+        print(f'Frame#{count}:SentBack@{sent_time}')
+        #---------------------------------
+        conn.send(meta)
+        
+        bbox_frame = cvDrawBoxes(detections, frame_resized)
+        #bbox_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        cv2.imshow(WIN_NAME, bbox_frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            print('Server is closed.')
+            break
+        
+        count += 1
+        
+    cv2.destroyAllWindows()
+    serversocket.close()
+    
+if __name__ == "__main__":
+    YOLO()
